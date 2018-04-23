fc2a547ff5da7470b1c154090fd8d4f50c7fd3ab
Nicolas Pearson <nic.s.pearson@gmail.com>
2018-04-23 20:58:39 +0200

    Added Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import './App.scss';
 
+import Button from 'material-ui/Button';
+
 import logo from './logo.svg';
 
 class App extends React.Component {
@@ -14,6 +16,10 @@ class App extends React.Component {
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <br />
+        <Button variant="raised" color="primary">
+          Hello World
+        </Button>
       </div>
     );
   }
